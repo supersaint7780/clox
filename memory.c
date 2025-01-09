7549fbf81bf013dd86a274365bc99740d01df059
Ayush <hackerboy5556@gmail.com>
2025-01-09 22:48:47 +0530

    bug fix

---
+++ memory.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        pointer = NULL;
+        return pointer;
+    }
+
+    void* temp = realloc(pointer, newSize);
+    if (temp == NULL) {
+        pointer = NULL;
+        exit(1);
+    }
+    pointer = temp;
+    return pointer;
+}
--- memory.h
+++ memory.h
@@ -12,20 +12,6 @@
 #define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * oldCount, 0)
 
-void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
-    if (newSize == 0) {
-        free(pointer);
-        pointer = NULL;
-        return pointer;
-    }
-
-    void* temp = realloc(pointer, newSize);
-    if (temp == NULL) {
-        pointer = NULL;
-        exit(1);
-    }
-    pointer = temp;
-    return pointer;
-}
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
 #endif
