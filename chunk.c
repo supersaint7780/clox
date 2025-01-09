c35617049fb3e8601d46ff5c68ba09e91e63f3d5
Ayush <hackerboy5556@gmail.com>
2025-01-09 22:35:02 +0530

    add constant pool

--- chunk.c
+++ chunk.c
@@ -7,6 +7,7 @@ void initChunk(Chunk* chunk) {
     chunk->capacity = 0;
     chunk->count = 0;
     chunk->code = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte) {
@@ -21,5 +22,13 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+
+    // Return the index where the constant was appended
+    return chunk->constants.count - 1;
 }
--- chunk.h
+++ chunk.h
@@ -2,6 +2,7 @@
 #define CLOX_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 /*
 * Each instruction will contain a one-byte operation code (opcode)
@@ -11,15 +12,31 @@ typedef enum {
     OP_RETURN, // Return from the current function
 } OpCode;
 
+/*
+* Many instruction sets store value directly in the code stream
+* right after the code. These are called immediate instructions.
+* For variable sized constants like string this does not work.
+* They are stored in a separate "constant data" region. The 
+* instruction to load the constant has an address/offset pointing
+* to where the value is stored in that section.
+*
+* For immediate instructions we also need to deal with alignment
+* padding and endianees. So instead for simplicty we put all
+* constant in the constant pool. Here ValueArray, each chunk has 
+* one.
+*/
+
 typedef struct {
     int count;
     int capacity;
-    uint8_t* code;
+    uint8_t* code; // bytecode array
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
 void freeChunk(Chunk* chunk);
+int addConstant(Chunk* chunk, Value value);
 
 
 #endif
